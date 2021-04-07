8fd9e86d8c368508390a0dfc539e1abfeb61b5d5
HieuPham2000 <hieu.pt183535@gmail.com>
2021-04-07 08:12:17 +0700

    upload: folder Lab03-JS

--- Lab03-JS/README.md
+++ Lab03-JS/README.md
@@ -4,16 +4,16 @@
 
 Gồm 2 phần: :rocket:
 
-1. Phần Practice :point_left:
-1.1. Array
-1.2. Open new window
-1.3. Count the Function calls
-1.4. Set controls' attributes
-1.5. Add text fields
-1.6. Validate PostalCode and Money (Regular Expression)  
+1. Phần Practice :point_left:  
+1.1. Array  
+1.2. Open new window  
+1.3. Count the Function calls  
+1.4. Set controls' attributes  
+1.5. Add text fields  
+1.6. Validate PostalCode and Money (Regular Expression)    
 
-2. Phần Exercise :point_left:
-2.1. Exercise 1 - Event Handling
-2.2. Exercise 2 - Inheritance
-2.3. Exercise 3 - DOM
-2.4. Exercise 4 - Validate inputs
+2. Phần Exercise :point_left:  
+2.1. Exercise 1 - Event Handling  
+2.2. Exercise 2 - Inheritance  
+2.3. Exercise 3 - DOM  
+2.4. Exercise 4 - Validate inputs  
---
+++ Lab03-JS/exercise/ex3-dom/script.js
@@ -0,0 +1,54 @@
+function addBlog() {
+  var nameElement = document.getElementById("right").contentWindow.document.getElementById("form-blog-name").name;
+  var name = nameElement.value;
+  if(name == "") {
+    return;
+  }
+  setCookie("blog-name", name, -1);
+  setCookie("blog-name", name, 1);
+  
+  setBlogName(getCookie("blog-name"));
+}
+
+function setCookie(cookie_name, cookie_value, expire_days) {
+  var d = new Date();
+  d.setTime(d.getTime() + (expire_days*24*60*60*1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = cookie_name + "=" + cookie_value + ";" + expires + ";path=/";
+}
+
+function getCookie(cookie_name) {
+  var name = cookie_name + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var arr = decodedCookie.split(';');
+  for(var i = 0; i <arr.length; i++) {
+    var c = arr[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function setBlogName(blogName) {
+  document.getElementById("left").contentWindow.document.getElementById("blog-name").innerHTML = blogName;
+}
+
+function loadLeftFrame() {
+  if(getCookie("blog-name")) {
+    setBlogName(getCookie("blog-name"));
+  } else {
+    setBlogName("You have not saved your favorite blog yet!");
+  }
+}
+
+function loadRightFrame() {
+  var right = document.getElementById("right");
+  var right_document = right.contentDocument || right.contentWindow.document;
+  right_document.getElementById("submit").addEventListener('click', addBlog);
+}
+
+//window.document.addEventListener('load', doLoad, false);
